3cd0bca97b515d7e305a08f14cef16d92d690100
Eduardo Bouhid <eduardobouhid.neto@gmail.com>
2025-01-26 16:21:09 -0300

    Updated lexer to return tokens instead of printing them. Also defined token codes.

---
+++ globals.h
@@ -0,0 +1,40 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+// Token definitions starting from 300
+#define IF 300
+#define ELSE 301
+#define WHILE 302
+#define INT 303
+#define VOID 304
+#define RETURN 305
+#define ID 306
+#define NUM 307
+#define PRC 308
+#define LBRACE 309
+#define RBRACE 310
+#define LPAREN 311
+#define RPAREN 312
+#define LBRACKET 313
+#define RBRACKET 314
+#define SEMICOLON 315
+#define DOT 316
+#define COMMA 317
+#define PLUS 318
+#define MINUS 319
+#define TIMES 320
+#define OVER 321
+#define MOD 322
+#define AND 323
+#define OR 324
+#define NOT 325
+#define NEQ 326
+#define EQ 327
+#define ASSIGN 328
+#define LT 329
+#define GT 330
+#define LEQ 331
+#define GEQ 332
+#define INVCHAR 333
+
+#endif // TOKENS_H
