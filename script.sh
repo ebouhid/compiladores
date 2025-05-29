dfc4867b884c783fbc4e1e5bc674b68617ff946f
dudubouneto <eduardobouhid.neto@gmail.com>
2025-05-28 21:04:52 -0300

    Included macos compile command

--- script.sh
+++ script.sh
@@ -5,7 +5,10 @@ flex src/lexical_analyser.l
 bison -d src/analise_sintatica.y
 
 # Compile the generated C code along with any additional source files
-gcc -g -o compiler lex.yy.c analise_sintatica.tab.c src/arvore.c src/tabSimbolos.c src/analise_semantica.c globals.h src/main.c src/codigo_intermediario.c -lfl
+gcc -g lex.yy.c analise_sintatica.tab.c src/arvore.c src/tabSimbolos.c src/analise_semantica.c src/main.c src/codigo_intermediario.c -ll -o compiler
+
+# If Linux, use the following command instead:
+# gcc -g -o compiler lex.yy.c analise_sintatica.tab.c src/arvore.c src/tabSimbolos.c src/analise_semantica.c globals.h src/main.c src/codigo_intermediario.c -lfl
 
 # Create a directory to store the output files
 if [ ! -d "outputs" ]; then
